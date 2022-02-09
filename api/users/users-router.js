c7ef4cef942e094dd421fc06dae1c6dcaaf00583
DeQuavion Wilburn <qua.thesage19@gmail.com>
2022-02-09 17:23:06 -0600

    Put request complete

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -30,10 +30,12 @@ router.post("/", logger, validateUser, (req, res, next) => {
     .catch(next);
 });
 
-router.put("/:id", (req, res) => {
-  // RETURN THE FRESHLY UPDATED USER OBJECT
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
+router.put("/:id", logger, validateUserId, validateUser, (req, res, next) => {
+  Users.update(req.params.id, req.body)
+    .then((user) => {
+      res.status(200).json(user);
+    })
+    .catch(next);
 });
 
 router.delete("/:id", (req, res) => {
