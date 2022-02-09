9236f8732871fc82fac566dff375edc1548f51ff
DeQuavion Wilburn <qua.thesage19@gmail.com>
2022-02-09 17:02:46 -0600

    Get all users

--- api/server.js
+++ api/server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const usersRouter = require("./users/users-router");
+const logger = require("morgan");
 
 const server = express();
 
@@ -6,6 +8,8 @@ const server = express();
 server.use(express.json());
 
 // global middlewares and the user's router need to be connected here
+server.use(logger("dev"));
+server.use("/api/users", usersRouter);
 
 server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
