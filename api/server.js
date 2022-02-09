fedcbf55111c5b778b706228e86a75594bf22cb5
DeQuavion Wilburn <qua.thesage19@gmail.com>
2022-02-09 16:36:56 -0600

    Server is running

--- api/server.js
+++ api/server.js
@@ -1,13 +1,20 @@
-const express = require('express');
+const express = require("express");
 
 const server = express();
 
 // remember express by default cannot parse JSON in request bodies
+server.use(express.json());
 
 // global middlewares and the user's router need to be connected here
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
+});
+
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+  });
 });
 
 module.exports = server;
